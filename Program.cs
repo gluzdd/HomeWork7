e568e36c2c11ad38afd6aaa4627f1cacb2bf39c0
Artem <Artem@Nesterov.ru>
2023-06-13 17:36:22 +0300

    HomeWork7

---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+
+Console.Write("Введите кол-во строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] resMatrix = GetMatrix(rows, columns, -99, 100);
+
+PrintMatrix(resMatrix);
+
+double[,] GetMatrix(int m, int n, int min, int max)
+{
+    double[,] matrix = new double[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1) /10.0;
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1,7 -> такого числа в массиве нет
+
+Console.Write("Введите кол-во строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите индекс строки: ");
+int rowsindex = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите индекс столбца: ");
+int columnsindex = Convert.ToInt32(Console.ReadLine());
+
+int[,] resMatrix = GetMatrix(rows, columns, 0, 10);
+if (rowsindex < resMatrix.GetLength(0) && columnsindex < resMatrix.GetLength(1))
+{
+    Console.WriteLine(resMatrix[rowsindex, columnsindex] + " - нужный индекс");
+}
+else
+{
+    Console.WriteLine($"{rowsindex}{columnsindex} - такого индекса в массиве нет");
+}
+
+PrintMatrix(resMatrix);
+Console.WriteLine();
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+    
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+Console.Write("Введите кол-во строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] resMatrix = GetMatrix(rows, columns, 0, 10);
+
+PrintMatrix(resMatrix);
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void MeanArithColumn(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(1); i++) 
+    {
+        double sum = 0; 
+        for (int j = 0; j < matr.GetLength(0); j++)
+        {
+            sum = sum + matr[j, i]; 
+        }
+    Console.WriteLine($"Среднее арифм. {i + 1} столбца = {sum / matr.GetLength(0)}");
+    }
+}
+Console.WriteLine();
+MeanArithColumn(resMatrix);
